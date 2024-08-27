c4e203ca709e3477ad50fea623f2a558d3c5a10d
SabikAbtahee <sabikchamp@gmail.com>
2024-08-27 08:14:24 +0600

    chore: testing passed

--- src/timings/controllers/timings.controller.spec.ts
+++ src/timings/controllers/timings.controller.spec.ts
@@ -1,18 +1,27 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { TimingsController } from './timings.controller';
+import { Test, TestingModule } from "@nestjs/testing";
+import { TimingsController } from "./timings.controller";
+import { TimingsService } from "../services/timings.service";
 
-describe('TimingsController', () => {
-  let controller: TimingsController;
+const mockTimingsService = {
+	getTimings: jest.fn()
+};
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [TimingsController],
-    }).compile();
+describe("TimingsController", () => {
+	let controller: TimingsController;
+    let service: TimingsService;
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+            controllers: [TimingsController],
+            providers: [
+                { provide: TimingsService, useValue: mockTimingsService },
+              ],
+		}).compile();
 
-    controller = module.get<TimingsController>(TimingsController);
-  });
+		controller = module.get<TimingsController>(TimingsController);
+        service = module.get<TimingsService>(TimingsService);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+	it("should be defined", () => {
+		expect(controller).toBeDefined();
+	});
 });
