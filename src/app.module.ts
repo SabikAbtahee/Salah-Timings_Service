2a02dc786829ecb4ad34f4dac9b6b6efb4729f6f
SabikAbtahee <sabikchamp@gmail.com>
2024-09-07 19:59:55 +0600

    feat: calendar get events made from ics url path

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { TimingsModule } from "./timings/timings.module";
 import { ConfigModule } from "@nestjs/config";
 import { BannerModule } from "./banner/banner.module";
 import { AuthModule } from './auth/auth.module';
+import { CalendarModule } from './calendar/calendar.module';
 
 @Module({
 	imports: [
 		TimingsModule,
 		BannerModule,
 		ConfigModule.forRoot({ envFilePath:`.env`,isGlobal: true }),
-		AuthModule
+		AuthModule,
+		CalendarModule
 	],
 	controllers: [AppController],
 	providers: [AppService]
